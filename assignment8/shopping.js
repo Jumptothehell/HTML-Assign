b023b616965555ef298606d703d96a8deccfecec
Cat say meaw <69138958+Jumptothehell@users.noreply.github.com>
2022-10-04 16:28:37 +0700

    +pageLoad (get obj from json)

--- assignment8/shopping.js
+++ assignment8/shopping.js
@@ -1,7 +1,15 @@
 window.onload = pageLoad;
 
 function pageLoad(){
-	
+	var xhr = new XMLHttpRequest();
+    xhr.open("GET", "cloth.json");
+    xhr.onload = function(){
+        var jsondata = JSON.parse(xhr.responseText);
+        console.log(jsondata);
+        display(jsondata);
+    };
+    xhr.onerror = function() { alert("ERROR!"); }; 
+    xhr.send();
 }
 
 function showData(data){
