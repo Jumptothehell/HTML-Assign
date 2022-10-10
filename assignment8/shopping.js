da165c7e64cfb3bd3467334507d3e61073ead46c
Cat say meaw <69138958+Jumptothehell@users.noreply.github.com>
2022-10-11 04:32:02 +0700

    Assign8 + commit2

--- assignment8/shopping.js
+++ assignment8/shopping.js
@@ -5,14 +5,26 @@ function pageLoad(){
     xhr.open("GET", "cloth.json");
     xhr.onload = function(){
         var jsondata = JSON.parse(xhr.responseText);
-        console.log(jsondata);
-        display(jsondata);
+        showData(jsondata);
     };
     xhr.onerror = function() { alert("ERROR!"); }; 
     xhr.send();
 }
 
 function showData(data){
-	
+    var allbox = document.querySelectorAll("#layer div");
+    var keys = Object.keys(data);
+    for(var i = 0; i < keys.length; i++)
+    {
+        var pic = document.createElement("img");
+        pic.id = "nav-top";
+        pic.src = "pic/" + [keys[i]] + ".jpg";
+        var Info = document.createElement("p")
+        Info.innerHTML = data[keys[i]].brandname + "<br>" + 
+        "price: " + data[keys[i]].price; 
+        
+        allbox[i].appendChild(pic);
+        allbox[i].appendChild(Info); 
+    }
 }
 
